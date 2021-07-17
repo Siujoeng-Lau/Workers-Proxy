667ce276a86fc1cf23aec4ec2936242ddafd3d84
lorenzoc25 <lorenzocao25@gmail.com>
2021-07-17 12:48:20 -0700

    removed test cases and reformat the useValidate

--- src/validate.ts
+++ src/validate.ts
@@ -2,6 +2,36 @@ import { ErrorOptions } from '../types/custom-error';
 import { FirewallOptions } from '../types/firewall';
 import { Middleware } from '../types/middleware';
 
+const validateFirewallOptions = (
+  userConfig: FirewallOptions,
+) : void => {
+  if (userConfig.field === undefined
+      || userConfig.operator === undefined
+      || userConfig.value === undefined) {
+    throw new Error('Invalid firewall configuration. Please check that you provided proper settings.');
+  }
+  const fields = new Set(['country', 'continent', 'asn', 'ip', 'hostname', 'user-agent']);
+  const operators = new Set(['equal', 'not equal', 'greater', 'less', 'in', 'not in', 'contain', 'not contain', 'match']);
+  if (fields.has(userConfig.field) === false) {
+    throw new Error('Invalid firewall field configuration');
+  }
+  if (operators.has(userConfig.operator) === false) {
+    throw new Error('Invalid firewall operator configuration');
+  }
+  if (Array.isArray(userConfig.value) === false) {
+    if (typeof userConfig.value !== 'string'
+          || typeof userConfig.value !== 'number') {
+      throw new Error('Invalid firewall value configuration');
+    }
+  }
+};
+
+const validateErrorOptions = (userConfig : ErrorOptions) : void => {
+  if (userConfig.errorCode === undefined || userConfig.responsePath === undefined) {
+    throw new Error('Invalid customized error configuration.');
+  }
+};
+
 export const useValidate: Middleware = (
   context,
   next,
@@ -11,67 +41,35 @@ export const useValidate: Middleware = (
     upstream,
     firewall,
     error,
-    // cors,
-    // optimization,
-    // header,
-    // security,
   } = options;
 
   if (upstream === undefined) {
-    throw new Error('Invalid upstream configuration. Please check again that you did not input empty value.');
+    throw new Error("The required field 'upstream' in the configuration is missing.");
   }
 
   if (Array.isArray(upstream)) {
-    upstream.forEach((userConfig) => {
-      if (userConfig.domain === undefined) {
-        throw new Error('Invalid domain configuration. Please check that you included domain in the configuration');
+    upstream.forEach((upstreamOptions) => {
+      if (upstreamOptions.domain === undefined) {
+        throw new Error("The required field 'domain' in the configuration is missing.");
       }
     });
   } else if (upstream.domain === undefined) {
-    throw new Error('Invalid domain configuration. Please check that you included domain in the configuration');
+    throw new Error("The required field 'domain' in the configuration is missing.");
   }
-
-  const checkFirewall = (userConfig: FirewallOptions) : void => {
-    if (userConfig.field === undefined
-        || userConfig.operator === undefined
-        || userConfig.value === undefined) {
-      throw new Error('Invalid firewall configuration. Please check that you provided proper settings.');
-    }
-    const fields = new Set(['country', 'continent', 'asn', 'ip', 'hostname', 'user-agent']);
-    const operators = new Set(['equal', 'not equal', 'greater', 'less', 'in', 'not in', 'contain', 'not contain', 'match']);
-    if (fields.has(userConfig.field) === false) {
-      throw new Error('Invalid firewall field configuration');
-    }
-    if (operators.has(userConfig.operator) === false) {
-      throw new Error('Invalid firewall operator configuration');
-    }
-    if (Array.isArray(userConfig.value) === false) {
-      if (typeof userConfig.value !== 'string'
-            || typeof userConfig.value !== 'number') {
-        throw new Error('Invalid firewall value configuration');
-      }
-    }
-  };
 
   if (firewall !== undefined) {
     if (Array.isArray(firewall)) {
-      firewall.forEach(checkFirewall);
+      firewall.forEach(validateFirewallOptions);
     } else {
-      checkFirewall(firewall);
+      validateFirewallOptions(firewall);
     }
   }
 
-  const checkError = (userConfig : ErrorOptions) : void => {
-    if (userConfig.errorCode === undefined || userConfig.responsePath === undefined) {
-      throw new Error('Invalid customized error configuration. ');
-    }
-  };
-
   if (error !== undefined) {
     if (Array.isArray(error)) {
-      error.forEach(checkError);
+      error.forEach(validateErrorOptions);
     } else {
-      checkError(error);
+      validateErrorOptions(error);
     }
   }
   return next();
