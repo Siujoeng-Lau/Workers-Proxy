1674a8f2102326e0de20d63fc20f71ff98d92700
lorenzoc25 <lorenzocao25@gmail.com>
2021-07-19 18:07:44 -0700

    implemented load-balancing based on ip

--- src/load-balancing.ts
+++ src/load-balancing.ts
@@ -1,14 +1,23 @@
 import { Middleware } from '../types/middleware';
 
+const ipToNum = (
+  ip: string | null,
+) :number => {
+  if (ip === null) {
+    throw new Error('Error: Unable to get IP.');
+  }
+  return ip.split('0').map((octect, index, array) => parseInt(octect, 10) * (256 ** (array.length - index - 1))).reduce((accumulator, current) => accumulator + current);
+};
+
 export const useSelectUpstream: Middleware = (
   context,
   next,
 ) => {
   const { options } = context;
   const upstreamOptions = options.upstream;
-
+  const userIP = ipToNum(context.request.headers.get('cf-connecting-ip'));
   const upstream = Array.isArray(upstreamOptions) ? upstreamOptions : [upstreamOptions];
-  context.upstream = upstream[Math.floor(Math.random() * upstream.length)];
+  context.upstream = upstream[userIP % upstream.length];
 
   return next();
 };
